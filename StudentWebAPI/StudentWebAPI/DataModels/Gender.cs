f49bcf8998f835638eab09617dbbb3ca31006acf
Resh2010 <reshma.ramesh@hrblock.com>
2022-08-24 17:39:21 +0530

    commit1

---
+++ StudentWebAPI/StudentWebAPI/DataModels/Gender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace StudentWebAPI.DataModels
+{
+    public class Gender
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+
+    }
+}
